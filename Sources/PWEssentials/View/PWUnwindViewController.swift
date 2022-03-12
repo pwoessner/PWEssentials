d0b8ae28e4c854a8962272ba4151e8c946c4ce43
Philipp Woessner <philipp.woessner@sap.com>
2022-03-12 14:30:27 +0100

    added PWUnwindViewController

---
+++ Sources/PWEssentials/View/PWUnwindViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PWUnwindViewController.swift
+//  
+//
+//  Created by Woessner, Philipp on 12.03.22.
+//
+
+import Foundation
+
+public protocol PWUnwindViewController {
+    func unwind()
+}
